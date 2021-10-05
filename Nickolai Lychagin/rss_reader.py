14a78b11462dbe0c6075c11837aff76d1e3ebdd2
Nickolai Lychagin <belarusconsul@gmail.com>
2021-10-06 02:03:27 +0300

    Version 1.0 - Iteration 1 completed

---
+++ Nickolai Lychagin/rss_reader.py
@@ -0,0 +1,261 @@
+#!/usr/bin/env python3
+
+"""
+Pure Python command-line RSS reader.
+
+This module:
+
+    - downloads information from RSS news feed
+    - processes various data from news items (title, description, date, image, link)
+    - prints data to console in text or JSON format
+
+Usage: rss_reader.py [-h] [--version] [--json] [--verbose] [--limit LIMIT] source
+
+    positional arguments:
+      source         RSS URL
+
+    optional arguments:
+      -h, --help     show this help message and exit
+      --version      Print version info
+      --json         Print result as JSON in stdout
+      --verbose      Outputs verbose status messages
+      --limit LIMIT  Limit news topics if this parameter provided
+
+Imported modules:
+
+    argparse
+    json
+    logging
+    re
+    xml.etree.ElementTree
+    StringIO from io
+    urlparse from urllib.parse
+    urlopen from urllib.request
+    URLError, HTTPError from urllib.error
+
+Functions:
+
+    parse_args() -> object
+    config_logging(verbose_flag: bool)
+    strip_html(text: str) -> str
+    download_xml(url: str) -> object
+    process_rss(root: object, limit: int/None) -> dict
+    dict_to_string(news_dict: dict, json_flag: bool) -> str
+    run_rss_reader()
+"""
+
+import argparse
+import json
+import logging
+import re
+import xml.etree.ElementTree as ET
+from io import StringIO
+from urllib.parse import urlparse
+from urllib.request import urlopen
+from urllib.error import URLError, HTTPError
+
+
+def parse_args():
+    """
+    Parse arguments from command-line input.
+
+    Returns:
+        args: argparse.Namespace - Namespace object with a dictionary of arguments.
+    """
+
+    parser = argparse.ArgumentParser(prog="rss_reader.py",
+                                     description="Pure Python command-line RSS reader.")
+    parser.add_argument("--version", action="version", version="Version 1.0",
+                        help="Print version info")
+    parser.add_argument("--json", action="store_true",
+                        help="Print result as JSON in stdout")
+    parser.add_argument("--verbose", action="store_true",
+                        help="Outputs verbose status messages")
+    parser.add_argument("--limit", type=int, default=None,
+                        help="Limit news topics if this parameter provided")
+    parser.add_argument("source", help="RSS URL")
+    args = parser.parse_args()
+    if args.limit is not None and args.limit < 1:
+        parser.error("argument --limit must be greater than 0")
+    return args
+
+
+def config_logging(verbose_flag):
+    """
+    Config logging object to log events to stdout.
+
+    Parameters:
+        verbose_flag: bool - True (logging level DEBUG) or False (logging level ERROR).
+    """
+
+    if verbose_flag:
+        logging.basicConfig(level=logging.DEBUG,
+                            format="%(levelname)s: %(asctime)s - %(message)s",
+                            datefmt="%d.%m.%Y %H:%M:%S")
+    else:
+        logging.basicConfig(level=logging.ERROR,
+                            format="%(levelname)s: %(message)s")
+
+
+def download_xml(url):
+    """
+    Download XML data from URL.
+
+    Parameters:
+        url: str - URL address.
+
+    Returns
+        root: xml.etree.ElementTree.Element - XML document as a tree.
+    """
+
+    url_parsed = urlparse(url)
+    if not all([url_parsed.netloc, url_parsed.scheme]):
+        logging.error(f"'{url}' does not seem to be a valid URL. Check that valid scheme is provided")
+        return None
+    try:
+        logging.info(f"Opening '{url}'")
+        with urlopen(url) as response:
+            root = ET.fromstring(response.read())
+            logging.info("XML root object created")
+            return root
+    except HTTPError as err:
+        logging.error(f"Download of '{url}' failed with error {err.code} - {err.reason}")
+    except URLError as err:
+        logging.error(f"Unable to open '{url}' due to error - {err.reason}")
+    except ET.ParseError:
+        logging.error(f"'{url}' is not a valid XML")
+    except UnicodeEncodeError:
+        logging.error("Source URL must contain only latin characters")
+
+
+def strip_html(text):
+    """
+    Remove HTML tags and spaces at the beginning and at the end of a string.
+
+    Parameters:
+        text: str - Any string of text.
+
+    Returns
+        str - String without HTML tags and spaces at the beginning and at the end.
+    """
+
+    return re.sub("<.+?>", "", text).strip()
+
+
+def process_rss(root, limit):
+    """
+    Process XML data from a RSS feed into a dictionary.
+
+    Parameters
+        root: xml.etree.ElementTree.Element - XML document as a tree.
+        limit: int/None - Number of news items to process. None - to process all items.
+
+    Returns
+        news_dict: dict - Dictionary of data from a RSS channel.
+    """
+
+    news_dict = {}
+    channel = root.find("channel")
+    if channel is None:
+        logging.error("RSS channel was not found in XML document")
+        return None
+    title = channel.find("title")
+    title = "" if title is None else strip_html(title.text)
+    logging.info(f"RSS channel '{title}' found")
+    desc = channel.find("description")
+    desc = "" if desc is None else strip_html(desc.text)
+    news_dict["Channel"] = {"Title": title, "Description": desc}
+    if channel.find("item") is None:
+        logging.error("No news found in RSS channel")
+        return None
+    n = 0
+    for item in channel.iter("item"):
+        if limit is not None and len(news_dict) > limit:
+            break
+        else:
+            n += 1
+            title = item.find("title")
+            title = "" if title is None else strip_html(title.text)
+            date = item.find("pubDate")
+            date = "" if date is None else strip_html(date.text)
+            image = item.find("enclosure")
+            image = "" if image is None else strip_html(image.attrib.get("url", ""))
+            desc = item.find("description")
+            desc = "" if desc is None else strip_html(desc.text)
+            link = item.find("link")
+            link = "" if link is None else strip_html(link.text)
+            news_dict[f"News {n}"] = {"Title": title,
+                                      "Date": date,
+                                      "Image": image,
+                                      "Description": desc,
+                                      "Link": link}
+    channel_length = len(news_dict) - 1
+    ending = "items" if channel_length > 1 else "item"
+    if limit is not None and channel_length < limit:
+        logging.warning(f"Limit set to {limit} but only {channel_length} news {ending} found")
+    logging.info(f"{channel_length} news {ending} processed")
+    return news_dict
+
+
+def dict_to_string(news_dict, json_flag):
+    """
+    Convert a dictionary of RSS data into a string.
+
+    Parameters:
+        news_dict: dict - Dictionary of data from a RSS channel.
+        json_flag: bool - True (convert to json string) or False (convert to text string).
+
+    Returns:
+        final_string: str - A string with information from a dictionary.
+    """
+
+    if json_flag:
+        final_string = json.dumps(news_dict, ensure_ascii=False, indent=4)
+        logging.info("Data converted to JSON string")
+    else:
+        string_obj = StringIO()
+        string_obj.write(f"\nFeed: {news_dict['Channel']['Title']}\n")
+        if news_dict["Channel"]["Description"]:
+            string_obj.write(f"Description: {news_dict['Channel']['Description']}\n")
+        string_obj.write("\n")
+        for item in news_dict:
+            if item.startswith("News"):
+                if news_dict[item]["Title"]:
+                    string_obj.write(f"Title: {news_dict[item]['Title']}\n")
+                if news_dict[item]["Date"]:
+                    string_obj.write(f"Date: {news_dict[item]['Date']}\n")
+                if news_dict[item]["Image"]:
+                    string_obj.write(f"Image: {news_dict[item]['Image']}\n")
+                if news_dict[item]["Description"]:
+                    string_obj.write(f"{news_dict[item]['Description']}\n")
+                if news_dict[item]["Link"]:
+                    string_obj.write(f"Read more: {news_dict[item]['Link']}\n")
+                string_obj.write("\n")
+        final_string = string_obj.getvalue()[:-1]
+        logging.info("Data converted to text string")
+    return final_string
+
+
+def run_rss_reader():
+    """
+    Main program logic of a RSS reader:
+        - parse arguments from command-line input
+        - config logging object
+        - download information from RSS news feed and process it
+        - print data to console in text or JSON format
+    """
+
+    args = parse_args()
+    config_logging(args.verbose)
+    root = download_xml(args.source)
+    if root is not None:
+        news_dict = process_rss(root, args.limit)
+        if news_dict is not None:
+            string_to_print = dict_to_string(news_dict, args.json)
+            logging.info("Printing information to stdout")
+            print(string_to_print)
+            logging.info("Program finished successfully")
+
+
+if __name__ == "__main__":
+    run_rss_reader()
---
+++ Nickolai Lychagin/tests.py
@@ -0,0 +1,304 @@
+#!/usr/bin/env python3
+
+"""
+Tests for rss_reader.py
+
+Usage:
+    python -m unittest discover
+
+    If coverage module installed:
+    coverage run -m unittest discover
+    coverage report -m
+
+Imported modules:
+
+    argparse
+    io
+    json
+    sys
+    xml.etree.ElementTree
+    unittest
+    patch from unittest.mock import
+
+Classes:
+
+    TestParser(unittest.TestCase)
+        Methods:
+            test_parse_args
+            test_limit_positive
+    TestErrors(unittest.TestCase)
+        Methods:
+            test_urlparse_error
+            test_url_error
+            test_http_error
+            test_etree_parse_error
+            test_unicode_encode_error
+            test_rss_channel_error
+            test_rss_item_error
+    TestVerbose(unittest.TestCase)
+        Methods:
+            test_log_info
+            test_log_warning
+    TestDictionary(unittest.TestCase)
+        Methods:
+            test_dictionary_keys
+            test_dictionary_length
+    TestStringMethods(unittest.TestCase)
+        Methods:
+            test_strip_html
+            test_dict_to_text_string
+            test_dict_to_json_string
+    TestPrinting(unittest.TestCase)
+        Methods:
+            test_print_text_string
+            test_print_json_string
+"""
+
+import argparse
+import io
+import json
+import sys
+import xml.etree.ElementTree as ET
+import unittest
+from unittest.mock import patch
+
+import rss_reader
+
+
+class TestParser(unittest.TestCase):
+    """
+    Tests for argparse functionality.
+
+    Methods:
+        test_parse_args:
+        test_limit_positive
+    """
+
+    @patch("argparse.ArgumentParser.parse_args",
+           return_value=argparse.Namespace(json=False, verbose=False,
+                                           limit=3, source="https://news.yahoo.com/rss/"))
+    def test_parse_args(self, mock_args):
+        """Test to assert that command-line input is parsed correctly"""
+        args = rss_reader.parse_args()
+        self.assertEqual(args.json, False)
+        self.assertEqual(args.verbose, False)
+        self.assertEqual(args.limit, 3)
+        self.assertEqual(args.source, "https://news.yahoo.com/rss/")
+
+    @patch('sys.stderr', new_callable=io.StringIO)
+    @patch("argparse.ArgumentParser.parse_args",
+           return_value=argparse.Namespace(json=False, verbose=False,
+                                           limit=0, source="https://news.yahoo.com/rss/"))
+    def test_limit_positive(self, mock_stderr, mock_args):
+        """Test to assert that 0 as --limit argument raises parser.error"""
+        with self.assertRaises(SystemExit):
+            rss_reader.parse_args()
+
+
+class TestErrors(unittest.TestCase):
+    """
+    Tests for catching errors and printing error messages.
+
+    Methods:
+        test_urlparse_error
+        test_url_error
+        test_http_error
+        test_etree_parse_error
+        test_unicode_encode_error
+        test_rss_channel_error
+        test_rss_item_error
+    """
+
+    @patch('logging.error')
+    def test_urlparse_error(self, mock_log):
+        """Test to assert that not valid URL is handled correctly"""
+        rss_reader.download_xml("a.com")
+        message = "'a.com' does not seem to be a valid URL. Check that valid scheme is provided"
+        mock_log.assert_called_with(message)
+
+    @patch('logging.error')
+    def test_url_error(self, mock_log):
+        """Test to assert that URLError is caught"""
+        rss_reader.download_xml("htt://google.com")
+        message = "Unable to open 'htt://google.com' due to error - unknown url type: htt"
+        mock_log.assert_called_with(message)
+
+    @patch('logging.error')
+    def test_http_error(self, mock_log):
+        """Test to assert that HTTPError is caught"""
+        rss_reader.download_xml("https://google.com/aaa")
+        message = "Download of 'https://google.com/aaa' failed with error 404 - Not Found"
+        mock_log.assert_called_with(message)
+
+    @patch('logging.error')
+    def test_etree_parse_error(self, mock_log):
+        """Test to assert that xml.etree.ElementTree.ParseError is caught"""
+        rss_reader.download_xml("https://google.com")
+        message = "'https://google.com' is not a valid XML"
+        mock_log.assert_called_with(message)
+
+    @patch('logging.error')
+    def test_unicode_encode_error(self, mock_log):
+        """Test to assert that UnicodeEncodeError is caught"""
+        rss_reader.download_xml("https://google.ком")
+        message = "Source URL must contain only latin characters"
+        mock_log.assert_called_with(message)
+
+    @patch('logging.error')
+    def test_rss_channel_error(self, mock_log):
+        """Test to assert that situation with no 'channel' tag in XML is handled correctly"""
+        xml_string = "<rss><channels></channels></rss>"
+        root = ET.fromstring(xml_string)
+        rss_reader.process_rss(root, 1)
+        message = "RSS channel was not found in XML document"
+        mock_log.assert_called_with(message)
+
+    @patch('logging.error')
+    def test_rss_item_error(self, mock_log):
+        """Test to assert that situation with no 'item' tag in XML is handled correctly"""
+        xml_string = "<rss><channel></channel></rss>"
+        root = ET.fromstring(xml_string)
+        rss_reader.process_rss(root, 1)
+        message = "No news found in RSS channel"
+        mock_log.assert_called_with(message)
+
+
+class TestVerbose(unittest.TestCase):
+    """
+    Tests for logging functionality in verbose mode.
+
+    Methods:
+        test_log_info
+        test_log_warning
+    """
+
+    @patch('logging.info')
+    def test_log_info(self, mock_log):
+        """Test to assert that messages at INFO level are logged to stdout in verbose mode"""
+        rss_reader.download_xml("https://news.yahoo.com/rss/")
+        message = "XML root object created"
+        mock_log.assert_called_with(message)
+
+    @patch('logging.warning')
+    def test_log_warning(self, mock_log):
+        """Test to assert that messages at WARNING level are logged to stdout in verbose mode"""
+        root = rss_reader.download_xml("https://www.independent.co.uk/news/uk/rss")
+        rss_reader.process_rss(root, 70)
+        mock_log.assert_called_once()
+
+
+class TestDictionary(unittest.TestCase):
+    """
+    Tests for correct dictionary construction.
+
+    Methods:
+        test_dictionary_keys
+        test_dictionary_length
+    """
+
+    def test_dictionary_keys(self):
+        """Test to assert that dictionary of RSS data has necessary keys"""
+        root = rss_reader.download_xml("https://news.yahoo.com/rss/")
+        news_dict = rss_reader.process_rss(root, 3)
+        for key in ["Channel", "News 1", "News 2", "News 3"]:
+            self.assertIn(key, news_dict.keys())
+        for key in ["Title", "Description"]:
+            self.assertIn(key, news_dict["Channel"].keys())
+        for key in ["Title", "Date", "Image", "Description", "Link"]:
+            self.assertIn(key, news_dict["News 1"].keys())
+
+    def test_dictionary_length(self):
+        """Test to assert that dictionary of RSS data is of correct length"""
+        root = rss_reader.download_xml("https://news.yahoo.com/rss/")
+        news_dict = rss_reader.process_rss(root, 4)
+        self.assertEqual(len(news_dict), 5)
+
+
+class TestStringMethods(unittest.TestCase):
+    """
+    Tests for correct functionality of string processing.
+
+    Methods:
+        test_strip_html
+        test_dict_to_text_string
+        test_dict_to_json_string
+    """
+
+    def test_strip_html(self):
+        """Test to assert that string is stripped of HTML tags and spaces at the beginning and at the end"""
+        text = "\n<div>Health secretary</div>  \n"
+        self.assertEqual(rss_reader.strip_html(text), "Health secretary")
+
+    def test_dict_to_text_string(self):
+        """Test to assert that dictionary is converted to text string correctly"""
+        test_dict = {'Channel': {'Title': 's1',
+                                 'Description': 's2'},
+                     'News 1': {'Title': 's3',
+                                'Date': 's4',
+                                'Image': 's5',
+                                'Description': 's6',
+                                'Link': 's7'}}
+        test_string = rss_reader.dict_to_string(test_dict, False)
+        compare_string = '\nFeed: s1\nDescription: s2\n\nTitle: s3\nDate: s4\nImage: s5\ns6\nRead more: s7\n'
+        self.assertEqual(test_string, compare_string)
+
+    def test_dict_to_json_string(self):
+        """Test to assert that dictionary is converted to JSON string correctly"""
+        test_dict = {'Channel': {'Title': 'Новости'}}
+        test_string = rss_reader.dict_to_string(test_dict, True)
+        compare_string = '{\n    "Channel": {\n        "Title": "Новости"\n    }\n}'
+        self.assertEqual(test_string, compare_string)
+
+
+class TestPrinting(unittest.TestCase):
+    """
+    Tests for printing to console functionality.
+
+    Methods:
+        test_print_text_string
+        test_print_json_string
+    """
+
+    @patch("argparse.ArgumentParser.parse_args",
+           return_value=argparse.Namespace(json=False, verbose=False,
+                                           limit=3, source="https://feeds.skynews.com/feeds/rss/home.xml"))
+    def test_print_text_string(self, mock_args):
+        """Test to assert that text string is printed to console correctly"""
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        rss_reader.run_rss_reader()
+        sys.stdout = sys.__stdout__
+        captured_output.seek(1)
+        self.assertIn("Sky News", captured_output.readline())
+        self.assertIn("Description: ", captured_output.readline())
+        captured_output.readline()
+        self.assertIn("Title: ", captured_output.readline())
+        self.assertIn("Date: ", captured_output.readline())
+        self.assertIn("Image: ", captured_output.readline())
+        captured_output.readline()
+        self.assertIn("Read more: ", captured_output.readline())
+
+    @patch("argparse.ArgumentParser.parse_args",
+           return_value=argparse.Namespace(json=True, verbose=True,
+                                           limit=3, source="https://feeds.skynews.com/feeds/rss/home.xml"))
+    def test_print_json_string(self, mock_args):
+        """Test to assert that JSON string is printed to console correctly"""
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        rss_reader.run_rss_reader()
+        sys.stdout = sys.__stdout__
+        captured_output.seek(2)
+        self.assertIn('    "Channel": {\n', captured_output.readline())
+        self.assertIn('        "Title"', captured_output.readline())
+        self.assertIn('        "Description"', captured_output.readline())
+        captured_output.readline()
+        self.assertIn('    "News 1": {\n', captured_output.readline())
+        self.assertIn('        "Title"', captured_output.readline())
+        self.assertIn('        "Date"', captured_output.readline())
+        self.assertIn('        "Image"', captured_output.readline())
+        self.assertIn('        "Description"', captured_output.readline())
+        self.assertIn('        "Link"', captured_output.readline())
+
+
+if __name__ == "__main__":
+    unittest.main()
